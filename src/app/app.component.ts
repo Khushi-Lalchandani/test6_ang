23ea924c852f80d41d225d268b0de81636cfb958
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2024-08-18 16:39:33 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'test6';
+export class AppComponent implements OnInit {
+  dataForm!: FormGroup;
+  genders = ['Male', 'Female', 'Others'];
+  ngOnInit(): void {
+    this.dataForm = new FormGroup({
+      fname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      lname: new FormControl(null, [
+        Validators.required,
+        Validators.maxLength(50),
+      ]),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.email,
+        Validators.maxLength(50),
+      ]),
+      country: new FormControl(null),
+      gender: new FormControl(null),
+    });
+  }
+  onSubmit() {
+    console.log(this.dataForm);
+    console.log(this.dataForm.controls['fname'].valid);
+  }
 }
