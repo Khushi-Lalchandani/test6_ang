78b90b7539f56918c692d7a49c630c71e1de75e5
Khushi-Lalchandani <140065096+Khushi-Lalchandani@users.noreply.github.com>
2024-08-20 18:47:02 +0530

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   dataForm!: FormGroup;
   genders = ['Male', 'Female', 'Others'];
+  valid = false;
   ngOnInit(): void {
     this.dataForm = new FormGroup({
       fname: new FormControl(null, [
@@ -29,6 +30,31 @@ export class AppComponent implements OnInit {
     });
   }
   onSubmit() {
-    console.log(this.dataForm);
+    this.dataForm.reset();
+  }
+
+  buttonDesign(): any {
+    if (this.dataForm.valid && this.valid === true) {
+      return {
+        cursor: 'pointer',
+        'box-shadow': '0 5px 16px 5px rgba(255,255,255,.2)',
+        transform: 'scale(1.04)',
+        padding: '10px 20px',
+        ' font-size': '15px',
+        'border-radius': '12px',
+        border: '1px solid #0cc5d2',
+        'background-color': '#0cc5d2',
+        color: ' #fff',
+      };
+    } else if (this.dataForm.valid && this.valid === false) {
+      return {
+        padding: '10px 20px',
+        ' font-size': '15px',
+        'border-radius': '12px',
+        border: '1px solid #0cc5d2',
+        'background-color': '#0cc5d2',
+        color: ' #fff',
+      };
+    }
   }
 }
